62c3ea0ab3de01ba27cef632144b09ea593488c7
Jones Torres <pj_jonesmartins@bocombbm.com.br>
2021-09-30 21:11:22 -0300

    Acertos na controller student

--- src/main/java/com/accenture/school/controller/StudentController.java
+++ src/main/java/com/accenture/school/controller/StudentController.java
@@ -12,19 +12,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.accenture.school.dto.StudentDto;
+import com.accenture.school.exception.BusinessException;
 import com.accenture.school.exception.RecordNotFoundException;
 import com.accenture.school.service.StudentService;
 
 @RestController
 public class StudentController {
 	@Autowired
-	StudentService studentService;
+	private StudentService studentService;
 	
 	//Controller route path
 	final String BASE_URL = "/students";
 	
 	@RequestMapping(value = BASE_URL + "/{id}", method = RequestMethod.GET, consumes = "application/json", produces = "application/json")
-	private ResponseEntity<StudentDto> getStudent(@PathVariable("id") long id) 
+	private ResponseEntity<StudentDto> getById(@PathVariable("id") long id) 
 	{
 		StudentDto studentDto = studentService.getById(id);
 		if(studentDto == null) {
@@ -34,7 +35,7 @@ public class StudentController {
 	}
 	
 	@RequestMapping(value = BASE_URL + "/{id}/classes", method = RequestMethod.GET, consumes = "application/json", produces = "application/json")
-	private ResponseEntity<StudentDto> getStudentWithClasses(@PathVariable("id") long id) 
+	private ResponseEntity<StudentDto> getByIdWithClasses(@PathVariable("id") long id) 
 	{
 		StudentDto studentDto = studentService.getByIdWithClass(id);
 		if(studentDto == null) {
@@ -54,22 +55,22 @@ public class StudentController {
 	}
 	
 	@RequestMapping(value = BASE_URL, method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-	private ResponseEntity<Long> createStudent(@RequestBody StudentDto dto) 
-	{
+	private ResponseEntity<Long> create(@RequestBody StudentDto dto) 
+	{	
 		long id = studentService.create(dto);
 		
 		return new ResponseEntity<Long>(id, HttpStatus.CREATED);
 	}
 
 	@RequestMapping(value = BASE_URL + "/{id}", method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
-	private ResponseEntity<Boolean> updateStudent(@RequestBody StudentDto dto, @PathVariable("id") long id) 
+	private ResponseEntity<Boolean> update(@RequestBody StudentDto dto, @PathVariable("id") long id) 
 	{
 		boolean updated = studentService.update(dto);
 		return new ResponseEntity<Boolean>(updated, HttpStatus.OK);
 	}
 	
 	@RequestMapping(value = BASE_URL + "/{id}", method = RequestMethod.DELETE, consumes = "application/json", produces = "application/json")
-	private ResponseEntity<Boolean> createStudent(@PathVariable("id") long id) 
+	private ResponseEntity<Boolean> delete(@PathVariable("id") long id) 
 	{
 		boolean deleted = studentService.delete(id);
 		return new ResponseEntity<Boolean>(deleted, HttpStatus.OK);
